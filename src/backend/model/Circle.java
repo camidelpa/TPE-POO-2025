82fb7cfbf3ceae88a12d5a12b635009a219a7949
Enzo Canelo <ecanelo@itba.edu.ar>
2025-12-16 16:46:29 -0300

    refactor: implement Figure logic in Circle

--- src/backend/model/Circle.java
+++ src/backend/model/Circle.java
@@ -1,9 +1,9 @@
 package backend.model;
 
-public class Circle extends Ellipse {
+public class Circle extends Figure {
 
-    protected final Point centerPoint;
-    protected final double radius;
+    private Point centerPoint;
+    private double radius;
 
     public Circle(Point centerPoint, double radius) {
         this.centerPoint = centerPoint;
@@ -11,8 +11,8 @@ public class Circle extends Ellipse {
     }
 
     @Override
-    public String toString() {
-        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, radius);
+    public String getFigureName() {
+        return "Círculo";
     }
 
     public Point getCenterPoint() {
@@ -23,4 +23,33 @@ public class Circle extends Ellipse {
         return radius;
     }
 
+    @Override
+    public String toString() {
+        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, radius);
+    }
+
+    // --- MÉTODOS OBLIGATORIOS DE FIGURE ---
+
+    @Override
+    public Point getCenter() {
+        // Para un círculo, el centro es su propiedad centerPoint
+        return centerPoint;
+    }
+
+    @Override
+    public boolean contains(Point point) {
+        // Lógica movida al backend: distancia menor al radio
+        return centerPoint.getDistanceTo(point) < radius;
+    }
+
+    @Override
+    public void move(double diffX, double diffY) {
+        centerPoint = new Point(centerPoint.getX() + diffX, centerPoint.getY() + diffY);
+    }
+
+    @Override
+    public Figure deepCopy() {
+        // Crea una copia nueva para la funcionalidad "Duplicar"
+        return new Circle(new Point(centerPoint.getX(), centerPoint.getY()), radius);
+    }
 }
--- src/backend/model/Rectangle.java
+++ src/backend/model/Rectangle.java
@@ -27,7 +27,8 @@ public class Rectangle extends Figure {
         return String.format("Rectángulo [ %s , %s ]", topLeft, bottomRight);
     }
 
-    // IMPLEMENTACIÓN NUEVA: Lógica matemática movida al backend
+    // --- IMPLEMENTACIÓN OBLIGATORIA DE FIGURE ---
+
     @Override
     public boolean contains(Point point) {
         return point.getX() > topLeft.getX() && point.getX() < bottomRight.getX() &&
@@ -38,5 +39,21 @@ public class Rectangle extends Figure {
     public void move(double diffX, double diffY) {
         topLeft = new Point(topLeft.getX() + diffX, topLeft.getY() + diffY);
         bottomRight = new Point(bottomRight.getX() + diffX, bottomRight.getY() + diffY);
+    }
+
+    @Override
+    public Point getCenter() {
+        // Calcula el promedio de X y el promedio de Y
+        double centerX = (topLeft.getX() + bottomRight.getX()) / 2;
+        double centerY = (topLeft.getY() + bottomRight.getY()) / 2;
+        return new Point(centerX, centerY);
+    }
+
+    @Override
+    public Figure deepCopy() {
+        return new Rectangle(
+                new Point(topLeft.getX(), topLeft.getY()),
+                new Point(bottomRight.getX(), bottomRight.getY())
+        );
     }
 }
