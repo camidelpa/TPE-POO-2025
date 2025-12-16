2cdb222687e76b0aebd4e7e2fb09a8a95ec9acbf
Enzo Canelo <ecanelo@itba.edu.ar>
2025-12-16 16:47:35 -0300

    refactor: implement Figure logic in Square

--- src/backend/model/Ellipse.java
+++ src/backend/model/Ellipse.java
@@ -1,9 +1,9 @@
 package backend.model;
 
-public class Ellipse implements Figure {
+public class Ellipse extends Figure {
 
-    protected final Point centerPoint;
-    protected final double sMayorAxis, sMinorAxis;
+    private Point centerPoint;
+    private double sMayorAxis, sMinorAxis; // Semiejes (radios)
 
     public Ellipse(Point centerPoint, double sMayorAxis, double sMinorAxis) {
         this.centerPoint = centerPoint;
@@ -12,8 +12,8 @@ public class Ellipse implements Figure {
     }
 
     @Override
-    public String toString() {
-        return String.format("Elipse [Centro: %s, DMayor: %.2f, DMenor: %.2f]", centerPoint, sMayorAxis, sMinorAxis);
+    public String getFigureName() {
+        return "Elipse";
     }
 
     public Point getCenterPoint() {
@@ -28,4 +28,34 @@ public class Ellipse implements Figure {
         return sMinorAxis;
     }
 
+    @Override
+    public String toString() {
+        return String.format("Elipse [Centro: %s, DMayor: %.2f, DMenor: %.2f]", centerPoint, sMayorAxis, sMinorAxis);
+    }
+
+    // --- IMPLEMENTACIÓN DE FIGURE ---
+
+    @Override
+    public Point getCenter() {
+        return centerPoint;
+    }
+
+    @Override
+    public boolean contains(Point point) {
+        // Fórmula matemática para saber si un punto está dentro de una elipse:
+        // (x - h)^2 / a^2 + (y - k)^2 / b^2 <= 1
+        double normalizedX = Math.pow(point.getX() - centerPoint.getX(), 2) / Math.pow(sMayorAxis, 2);
+        double normalizedY = Math.pow(point.getY() - centerPoint.getY(), 2) / Math.pow(sMinorAxis, 2);
+        return (normalizedX + normalizedY) <= 1.0;
+    }
+
+    @Override
+    public void move(double diffX, double diffY) {
+        centerPoint = new Point(centerPoint.getX() + diffX, centerPoint.getY() + diffY);
+    }
+
+    @Override
+    public Figure deepCopy() {
+        return new Ellipse(new Point(centerPoint.getX(), centerPoint.getY()), sMayorAxis, sMinorAxis);
+    }
 }
--- src/backend/model/Square.java
+++ src/backend/model/Square.java
@@ -1,12 +1,12 @@
 package backend.model;
 
-public class Square implements Figure {
+public class Square extends Figure {
 
-    private final Point topLeft, bottomRight;
+    private Point topLeft, bottomRight;
 
     public Square(Point topLeft, double size) {
         this.topLeft = topLeft;
-        this.bottomRight = new Point(topLeft.x + size, topLeft.y + size);
+        this.bottomRight = new Point(topLeft.getX() + size, topLeft.getY() + size);
     }
 
     public Point getTopLeft() {
@@ -18,8 +18,41 @@ public class Square implements Figure {
     }
 
     @Override
+    public String getFigureName() {
+        return "Cuadrado";
+    }
+
+    @Override
     public String toString() {
         return String.format("Cuadrado [ %s , %s ]", topLeft, bottomRight);
     }
 
+    // --- IMPLEMENTACIÓN DE FIGURE ---
+
+    @Override
+    public boolean contains(Point point) {
+        // La lógica es igual a la del rectángulo
+        return point.getX() > topLeft.getX() && point.getX() < bottomRight.getX() &&
+                point.getY() > topLeft.getY() && point.getY() < bottomRight.getY();
+    }
+
+    @Override
+    public void move(double diffX, double diffY) {
+        topLeft = new Point(topLeft.getX() + diffX, topLeft.getY() + diffY);
+        bottomRight = new Point(bottomRight.getX() + diffX, bottomRight.getY() + diffY);
+    }
+
+    @Override
+    public Point getCenter() {
+        double centerX = (topLeft.getX() + bottomRight.getX()) / 2;
+        double centerY = (topLeft.getY() + bottomRight.getY()) / 2;
+        return new Point(centerX, centerY);
+    }
+
+    @Override
+    public Figure deepCopy() {
+        // Calculamos el tamaño original para crear uno nuevo
+        double size = Math.abs(bottomRight.getX() - topLeft.getX());
+        return new Square(new Point(topLeft.getX(), topLeft.getY()), size);
+    }
 }
