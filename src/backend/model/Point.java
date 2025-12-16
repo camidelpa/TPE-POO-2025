f363d8dd1666698c9097b4685b4b13c0d7f2a202
Enzo Canelo <ecanelo@itba.edu.ar>
2025-12-16 16:06:16 -0300

    feat: add getDistance for Circle.java

--- src/backend/model/Point.java
+++ src/backend/model/Point.java
@@ -2,16 +2,29 @@ package backend.model;
 
 public class Point {
 
-    public double x, y;
+    private final double x, y;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    // pitagoras to calculate distance between this point and another point
+    // needed to know if theres a click on te circle ;)
+    public double getDistanceTo(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+
     @Override
     public String toString() {
         return String.format("{%.2f , %.2f}", x, y);
     }
-
 }
