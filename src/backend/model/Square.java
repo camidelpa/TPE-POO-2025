41bb90c5318243aabf3997ca31970f4d599b9079
Lucasagus285 <lucasagus285@gmail.com>
2025-12-16 19:15:47 -0300

    Modificacion a square
    Square ahora es hijo de Rectangle, no de figure.

--- src/backend/model/Square.java
+++ src/backend/model/Square.java
@@ -1,58 +1,12 @@
 package backend.model;
 
-public class Square extends Figure {
+public class Square extends Rectangle {
 
-    private Point topLeft, bottomRight;
+    private final String figureName = "Cuadrado";
 
     public Square(Point topLeft, double size) {
-        this.topLeft = topLeft;
-        this.bottomRight = new Point(topLeft.getX() + size, topLeft.getY() + size);
+        super(topLeft, new Point(topLeft.getX() + size, topLeft.getY() + size));
     }
 
-    public Point getTopLeft() {
-        return topLeft;
-    }
-
-    public Point getBottomRight() {
-        return bottomRight;
-    }
-
-    @Override
-    public String getFigureName() {
-        return "Cuadrado";
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Cuadrado [ %s , %s ]", topLeft, bottomRight);
-    }
-
-    // --- IMPLEMENTACIÓN DE FIGURE ---
-
-    @Override
-    public boolean contains(Point point) {
-        // La lógica es igual a la del rectángulo
-        return point.getX() > topLeft.getX() && point.getX() < bottomRight.getX() &&
-                point.getY() > topLeft.getY() && point.getY() < bottomRight.getY();
-    }
-
-    @Override
-    public void move(double diffX, double diffY) {
-        topLeft = new Point(topLeft.getX() + diffX, topLeft.getY() + diffY);
-        bottomRight = new Point(bottomRight.getX() + diffX, bottomRight.getY() + diffY);
-    }
-
-    @Override
-    public Point getCenter() {
-        double centerX = (topLeft.getX() + bottomRight.getX()) / 2;
-        double centerY = (topLeft.getY() + bottomRight.getY()) / 2;
-        return new Point(centerX, centerY);
-    }
-
-    @Override
-    public Figure deepCopy() {
-        // Calculamos el tamaño original para crear uno nuevo
-        double size = Math.abs(bottomRight.getX() - topLeft.getX());
-        return new Square(new Point(topLeft.getX(), topLeft.getY()), size);
-    }
+    // Implementacion de figure ya esta hecha en el padre (Rectangle)
 }
